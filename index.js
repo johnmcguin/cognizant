10d631fdce7fe0c029cba73f5378d79ec740888f
John <johnmcguin88@gmail.com>
2018-03-08 19:22:11 -0700

    logging ascii art

--- index.js
+++ index.js
@@ -2,4 +2,9 @@
 'use strict';
 const { exec } = require('child_process');
 const args = process.argv.slice(2); // ignore first two args: [node, path to executable]
+const { readFile } = require('fs');
 // 
+let file = readFile('./nirvana.txt', 'ascii', (err, results) => {
+    console.log(err);
+    console.log(results);
+});
