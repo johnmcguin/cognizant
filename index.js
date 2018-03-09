d4330fc0f9e8b96898cbe7043bddf5fc287ce550
John <johnmcguin88@gmail.com>
2018-03-08 19:04:10 -0700

    add shebang line

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-// 'use strict';
-// const { exec } = require('child_process');
-// const args = process.argv;
-
-console.log('JELLO WORLD!!');
-console.log(args);
+#! /usr/bin/env node
+'use strict';
+const { exec } = require('child_process');
+const args = process.argv.slice(2); // ignore first two args: [node, path to executable]
+// 
