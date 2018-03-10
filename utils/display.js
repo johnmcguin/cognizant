fcc747c9b23ac055d4409a82926b0adf118be4f8
John <johnmcguin88@gmail.com>
2018-03-10 15:24:57 -0700

    whoops

--- utils/display.js
+++ utils/display.js
@@ -5,7 +5,7 @@ const log = console.log;
 module.exports = { displayArt };
 
 function displayArt() {
-    readFile('./cognizant.txt', 'ascii', (err, results) => {
+    readFile('../cognizant.txt', 'ascii', (err, results) => {
         log('\n', '\n', '\n', '\n');
         log(chalk.cyanBright(results));
         log('\n', '\n', '\n', '\n');
